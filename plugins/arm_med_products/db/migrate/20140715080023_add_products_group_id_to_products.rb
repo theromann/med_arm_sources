7597fc36809bc915f639640793a52338cb8c3806
Romanychev Andrey <dron08@mail.ru>
2014-07-15 17:38:25 +0400

    delete unused migration. complited product_index. #200

--- plugins/arm_med_products/db/migrate/20140715080023_add_products_group_id_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddProductsGroupIdToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :products_group_id, :integer
-  end
-end
--- plugins/arm_status/db/migrate/20140715081221_add_status_id_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusIdToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :status_id, :integer
-  end
-end
