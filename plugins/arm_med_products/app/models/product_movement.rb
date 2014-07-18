ecf49112a966c1450b9dd916fd9769347c7cc0c5
Romanychev Andrey <dron08@mail.ru>
2014-07-18 07:39:22 +0400

    used cocoon scripts. #277

--- plugins/arm_med_products/app/models/product_movement.rb
+++ plugins/arm_med_products/app/models/product_movement.rb
@@ -1,5 +1,13 @@
 class ProductMovement < ActiveRecord::Base
   unloadable
+  include Redmine::SafeAttributes
+
   has_many :product_storage_relations
-  accepts_nested_attributes_for :product_storage_relations, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :product_storage_relations, :allow_destroy => true
+
+
+  safe_attributes 'name',
+                  'product_storage_relations_attributes'
+
+
 end
